2f14ba27741a04bfe9b30f19332aff103bc56aec
JAMO2001 <jorge_martinez3@baylor.edu>
2023-12-13 14:11:10 -0500

    updated display function

--- src/fdraw.m
+++ src/fdraw.m
@@ -4,21 +4,28 @@ arguments
     Y
     U
     args.title = []
-    args.color_label = []
+    args.bar_lim = [0 1]
+    args.bar_ticks = [];
 end
 %DRAW Draws the solution
     surf(X,Y,U,'EdgeColor','none')
-    c = colorbar;
+    
     view(2)
     axis equal
+    c = colorbar;
     xlim([0 9])
     ylim([0 9])
     xlabel('x')
     ylabel('y')
-    clim([0 1])
+    
+    if ~isempty(args.bar_lim)
+        clim(args.bar_lim)
+    end
 
-    if ~isempty(args.color_label)
-        c.Label.String = args.color_label;
+    if ~isempty(args.bar_ticks)
+        ticks = linspace(c.Limits(1),c.Limits(2),length(args.bar_ticks));
+        c.Ticks = ticks;
+        c.TickLabels = args.bar_ticks;
     end
 
     if ~isempty(args.title)
