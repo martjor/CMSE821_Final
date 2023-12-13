a6bfb7bebca8b5e09a7ba2fa63495ec46877863a
JAMO2001 <jorge_martinez3@baylor.edu>
2023-12-13 13:13:53 -0500

    updated visualization

--- src/animate.m
+++ src/animate.m
@@ -7,11 +7,11 @@ arguments
 end
 %ANIMATE Creates an animation of the evolving crystal
     % Discretize time
-    t = t_span(1):k:t_span(2);
-    n = length(t);
+    t_span = t_span(1):k:t_span(2);
+    n = length(t_span);
     draw_rate = floor(n * options.draw_rate);
 
-    for i = 1:length(t)
+    for i = 1:length(t_span)
         if i == 1
             phi = simulation.phi_naught;
             T = simulation.T_naught;
@@ -19,14 +19,38 @@ end
             [phi,T] = simulation.step(phi,T,k);
         end
         
-        if mod(i-1,draw_rate) == 0
-            subplot(1,2,1)
-            fdraw(simulation.x,simulation.y,phi,"title",sprintf("t=%f",t(i)))
+        if mod(i-1,draw_rate) == 0   
+            % Create tiled layout
+            if i == 1
+                tiledlayout('horizontal');
+                
+                ax_phi = nexttile;
+                fdraw(simulation.x,simulation.y,phi,"title","\phi")
+                colormap(ax_phi,"parula")
+     
+                ax_T = nexttile;
+                fdraw(simulation.x,simulation.y,T,"title","T")
+                ylabel("")
+                yticklabels([])
+                colormap(ax_T,"hot")
 
-            subplot(1,2,2)
-            fdraw(simulation.x,simulation.y,T,"title",sprintf("t=%f",t(i)))
-            pause(0.1)
-            drawnow
+                t = text(ax_phi,ax_phi.XLim(2) * 0.10,...
+                                ax_phi.YLim(2) * 0.90,...
+                                sprintf("t=%.2f",t_span(i)),...
+                                'Color','m',...
+                                'FontSize',12);
+            end
+
+            % Update phi
+            set(ax_phi.Children(2),"ZData",phi);
+
+            % Update T
+            set(ax_T.Children,"ZData",T);
+
+            % Update label
+            t.String = sprintf("t=%.2f",t_span(i));
+
+            drawnow limitrate         
         end
     end
 end
--- src/fdraw.m
+++ src/fdraw.m
@@ -4,14 +4,22 @@ arguments
     Y
     U
     args.title = []
+    args.color_label = []
 end
 %DRAW Draws the solution
     surf(X,Y,U,'EdgeColor','none')
-    colorbar
+    c = colorbar;
     view(2)
     axis equal
     xlim([0 9])
     ylim([0 9])
+    xlabel('x')
+    ylabel('y')
+    clim([0 1])
+
+    if ~isempty(args.color_label)
+        c.Label.String = args.color_label;
+    end
 
     if ~isempty(args.title)
         title(args.title)
